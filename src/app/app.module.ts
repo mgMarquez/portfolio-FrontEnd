4145a10bbaf9bf29e40c01c01535e79dd5e4bd66
Mauricio Marquez <mgmarquezsilva@gmail.com>
2022-08-15 20:46:34 -0300

    componentes educacion, aptitudes, logros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './components/encabezado/encabezado.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { AptitudesComponent } from './components/aptitudes/aptitudes.component';
+import { LogrosComponent } from './components/logros/logros.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EncabezadoComponent,
     AcercaDeComponent,
-    ExperienciaComponent
+    ExperienciaComponent,
+    EducacionComponent,
+    AptitudesComponent,
+    LogrosComponent
   ],
   imports: [
     BrowserModule
